0abadb27ffcd03391360124f1ad349e6b584e83b
ryn-9 <mbinroy@gmail.com>
2026-06-10 15:07:57 +0700

    limit client

--- config/db.js
+++ config/db.js
@@ -1,11 +1,18 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
+  max: 3,
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 10000,
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected database error:", err);
 });
 
 module.exports = pool;
